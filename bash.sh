ead4686ef0eb03fdf515de5705ae5b4a19546c81
Yann Zerlaut <yann.zerlaut@icm-institute.org>
2022-02-09 17:58:47 +0100

    adding bash

---
+++ bash.sh
@@ -0,0 +1,5 @@
+python code/input-processing.py V1 &
+python code/input-processing.py V2 &
+python code/input-processing.py V2-CB1-KO &
+python code/input-processing.py V2-no-CB1-L4 &
+sleep 300s; python code/input-processing.py plot
