20cc96c1a6debe8becda8cd3064abe9fef56714b
Yann Zerlaut <yann.zerlaut@icm-institute.org>
2022-02-14 18:40:42 +0100

    update bash

--- bash.sh
+++ bash.sh
@@ -1,6 +1,6 @@
-source $HOME/.bashrc
-python code/input-processing.py V1 &
-python code/input-processing.py V2 &
-python code/input-processing.py V2-CB1-KO &
-python code/input-processing.py V2-no-CB1-L4 &
-#sleep 300s; python code/input-processing.py plot
+python_path='/home/yann.zerlaut/miniconda3/bin/python'
+$python_path code/input-processing.py V1 &
+$python_path code/input-processing.py V2 &
+$python_path code/input-processing.py V2-CB1-KO &
+$python_path code/input-processing.py V2-no-CB1-L4 &
+#sleep 300s; $python_path code/input-processing.py plot
